6f53cfb248b1833734958d2c3d533a5fd6dcb1b0
alvinfdl21 <alvinfdl21@gmail.com>
2023-11-29 11:47:54 +0700

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,56 @@
+export const info = {
+  baseUrl: "https://alvin.deno.dev",
+  name: "Alvin Fadli Dwi Mulya",
+  jobDescription: "Software Engineer",
+  about: `Final year software engineering student who passionate about all things technology and software development. My particular area of interest lies in backend development, where I enjoy working with databases, APIs, and other backend technologies to build robust and efficient systems.
+  `,
+
+  experience: [
+    {
+      name: "Sagara Technology",
+      location: "Bandung, Indonesia",
+      startDate: "Aug 2023",
+      endDate: "Nov 2023",
+      description: [
+        "• Created RESTful API for Teknologi.id using Laravel, Node.JS and PostgreSQL",
+        "• Developed user interfaces (UI) for Teknologi.id using Vue.js and the Quasar Framework.",
+        "• Updated minor features for Teknologi.id backend using Node.JS.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Politeknik Negeri Padang",
+      location: "Padang, Indonesia",
+      startDate: "Aug 2020",
+      endDate: "Present",
+      description: [
+        "Bachelor of Applied Sciences,  Software Engineering Technology",
+      ],
+    },
+    {
+      name: "IU International University of Applied Sciences",
+      location: "Bad Honnef, Germany",
+      startDate: "Oct 2022",
+      endDate: "Mar 2023",
+      description: ["Exchange Student, Computer Sciences"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/alvinfadli",
+    email: "mailto:alvinfdl21gmail.com",
+    linkedin: "https://www.linkedin.com/in/alvinfadli/",
+  },
+
+  projects: [
+    {
+      title: "Gigs",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "",
+      liveUrl: "",
+    },
+  ],
+};
